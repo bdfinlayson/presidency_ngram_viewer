bac194311b9b7e7c31641fa4f6a9d61ffb774027
Bryan Finlayson <bdfinlayson@gmail.com>
2022-01-13 21:39:20 -0600

    get initial plot working

--- tokenizer/ngram_table_aggregator.R
+++ tokenizer/ngram_table_aggregator.R
@@ -9,14 +9,18 @@ table_names <- db_list_tables(con)
 new_df <- data.frame(
     ngrams_total = character(),
     Freq = integer(),
-    year = character(),
+    year = integer(),
     president = character(),
-    ngram_length = integer()
+    ngram_length = integer(),
+    total_ngrams_for_year = integer()
   )
 
 for (table_name in table_names) {
-  table <- tbl(con, table_name)
-  new_df <- bind_rows(new_df, as.data.frame(table))
+  table <- tbl(con, table_name) %>% 
+    as.data.frame() %>% 
+    mutate(year = as.integer(year)) %>% 
+    mutate(total_ngrams_for_year = sum(Freq))
+  new_df <- bind_rows(new_df, table)
   print(c("APPENDED Table", table_name))
   print(c("TOTAL Rows", nrow(new_df)))
 }
@@ -29,6 +33,6 @@ new_df <- new_df %>%
 
 print(c('CREATING NEW TABLE WITH', nrow(new_df), 'rows'))
 
-db_create_table(con, 'all_ngrams', new_df)
+db_create_table(con, 'all_ngrams', new_df, overwrite = TRUE)
 
 print('TABLE CREATED')
